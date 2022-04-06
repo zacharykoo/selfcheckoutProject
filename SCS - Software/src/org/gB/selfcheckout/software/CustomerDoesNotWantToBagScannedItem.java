107e8370e4f3b9416b3c79362b07dc3d3015d433
tim-lerm <timofei.lerman@ucalgary.ca>
2022-04-06 09:35:32 -0600

    Refactored classes.

--- SCS - Software/src/org/gB/selfcheckout/software/CustomerDoesNotWantToBagScannedItem.java
+++ SCS - Software/src/org/gB/selfcheckout/software/CustomerDoesNotWantToBagScannedItem.java
@@ -5,42 +5,52 @@ import org.lsmr.selfcheckout.devices.ElectronicScale;
 import org.lsmr.selfcheckout.devices.observers.AbstractDeviceObserver;
 import org.lsmr.selfcheckout.devices.observers.ElectronicScaleObserver;
 
-public class CustomerDoesNotBagScannedItem implements ElectronicScaleObserver {
+public class CustomerDoesNotWantToBagScannedItem implements ElectronicScaleObserver {
 	private State state;  // The state of the self checkout station's software.
 	private boolean enabled; // Indicates whether the watched device is enabled.
 
 	
-	public CustomerDoesNotBagScannedItem(State state) {
+	public CustomerDoesNotWantToBagScannedItem(State state) {
 		this.state = state;
 		this.enabled = true;
 	}
 	@Override
 	public void enabled(AbstractDevice<? extends AbstractDeviceObserver> device) {
-		// TODO Auto-generated method stub
+		this.enabled = true;
 		
 	}
 
 	@Override
 	public void disabled(AbstractDevice<? extends AbstractDeviceObserver> device) {
-		// TODO Auto-generated method stub
+		this.enabled = false;
 		
 	}
 
 	@Override
 	public void weightChanged(ElectronicScale scale, double weightInGrams) {
-		// TODO Auto-generated method stub
-		
+		if (!this.enabled) return;
+		if (state.getExpectedWeight() == weightInGrams) {
+				// then re-enable the scanning status.
+				state.scs.mainScanner.enable();
+				state.scs.handheldScanner.enable();
+				state.waitingForBagging = false;
+			}
+			
+		// Issue an error to the interface for an unprompted weight change.
+		else if (state.getExpectedWeight() != weightInGrams)
+			Main.error("Unexpected weight detected.");
 	}
 
 	@Override
 	public void overload(ElectronicScale scale) {
-		// TODO Auto-generated method stub
-		
+		if (this.enabled)
+			Main.error("Scale overloaded.");
 	}
 
 	@Override
 	public void outOfOverload(ElectronicScale scale) {
-		// TODO Auto-generated method stub
+		if (this.enabled)
+			Main.error("Scale no longer overloaded.");
 		
 	}
 
