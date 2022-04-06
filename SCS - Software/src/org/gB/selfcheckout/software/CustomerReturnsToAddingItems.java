6b1ccb94447b7bc696b6f7f7ddbf16ebe8693985
tim-lerm <timofei.lerman@ucalgary.ca>
2022-04-06 14:12:25 -0600

    Merge branch 'Backend/Usecase-Group-1'

--- SCS - Software/src/org/gB/selfcheckout/software/CustomerReturnsToAddingItems.java
+++ SCS - Software/src/org/gB/selfcheckout/software/CustomerReturnsToAddingItems.java
@@ -1,5 +1,31 @@
 package org.gB.selfcheckout.software;
 
-public class CustomerReturnsToAddingItems {
+import org.lsmr.selfcheckout.devices.AbstractDevice;
+import org.lsmr.selfcheckout.devices.observers.AbstractDeviceObserver;
+import org.lsmr.selfcheckout.devices.observers.TouchScreenObserver;
+
+public class CustomerReturnsToAddingItems implements TouchScreenObserver{
+	private State state;  // The state of the self checkout station's software.
+	private boolean enabled; // Indicates whether the watched device is enabled.
+	private boolean returnToScan;
+
+	
+	public CustomerReturnsToAddingItems(State state) {
+		this.state = state;
+		this.enabled = true;
+		this.returnToScan = false;
+	}
+	@Override
+	public void enabled(AbstractDevice<? extends AbstractDeviceObserver> device) {
+		this.enabled = true;
+		this.returnToScan = true;
+	}
+
+	@Override
+	public void disabled(AbstractDevice<? extends AbstractDeviceObserver> device) {
+		this.enabled = false;
+		
+	}
+	
 
 }
