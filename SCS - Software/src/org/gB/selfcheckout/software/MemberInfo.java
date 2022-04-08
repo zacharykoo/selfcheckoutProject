e4e2f9145d474672f5dc19f2dbe7d669cb5538e0
Miro <47343299+nir04m@users.noreply.github.com>
2022-04-08 00:20:36 -0600

    3 frontend files

---
+++ SCS - Software/src/org/gB/selfcheckout/software/MemberInfo.java
@@ -0,0 +1,25 @@
+import javax.swing.*;  
+import java.awt.*;  
+import java.awt.event.*; 
+    public class MemberInfo extends JFrame implements ActionListener{
+    	public static void main(String[] args) {
+    		JFrame frame= new JFrame("Membership Info");  
+            JTextField t1;  
+            t1=new JTextField("Enter your member info");  
+            t1.setBounds(90,100, 250,30);
+            JLabel l1;
+    	    l1=new JLabel("Member info :");
+    	    l1.setBounds(10,105, 190,20);
+    	    frame.add(l1);
+            frame.add(t1);  
+            frame.setSize(400,400);  
+            frame.setLayout(null);  
+            frame.setVisible(true); 
+    	}
+    	@Override
+		public void actionPerformed(ActionEvent e) {
+			// TODO Auto-generated method stub
+			
+		} 
+         
+    }
