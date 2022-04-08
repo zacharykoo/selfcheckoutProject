7c0d1767cf3bf740e191310e326b831c37fb6c73
SummerChu <kennyannvo2@hotmail.com>
2022-04-07 21:01:35 -0600

    Clean Unused code
    Add Button and text field is removed

--- SCS - Software/src/org/gB/selfcheckout/software/UI/AttendantCartScreen.java
+++ SCS - Software/src/org/gB/selfcheckout/software/UI/AttendantCartScreen.java
@@ -14,8 +14,7 @@ public class AttendantCartScreen extends JPanel implements ListSelectionListener
     //TODO: Modify to accomodate for actual items
     private JList<String> items;
     private DefaultListModel<String> itemModel;
-    private JTextField itemName;
-    private JButton addButton, removeButton, backButton;
+    private JButton removeButton, backButton;
     public AttendantCartScreen() {
         this.setLayout(new GridBagLayout());
         this.setPreferredSize(new Dimension(500, 500));
@@ -45,29 +44,16 @@ public class AttendantCartScreen extends JPanel implements ListSelectionListener
         c.gridy = 0;
         this.add(listScrollPane, c);
         
-        /*
-        addButton = new JButton("Add");
-        AddListener addListener = new AddListener(addButton);
-        addButton.setActionCommand("Add");
-        addButton.addActionListener(addListener);
-        addButton.setEnabled(false);
-        */
-
         removeButton = new JButton("Remove");
-        RemoveListener removeListener = new RemoveListener(removeButton);
+        RemoveListener removeListener = new RemoveListener();
         removeButton.setActionCommand("Remove");
         removeButton.addActionListener(removeListener);
 
         backButton = new JButton("Back");
-        BackListener backListener = new BackListener(backButton);
+        BackListener backListener = new BackListener();
         backButton.setActionCommand("Back");
         backButton.addActionListener(backListener);
 
-        /*
-        itemName = new JTextField(10);
-        itemName.addActionListener(addListener);
-        itemName.getDocument().addDocumentListener(addListener);
-        */
         String name = itemModel.getElementAt(items.getSelectedIndex()).toString();
         JPanel buttonPane = new JPanel();
         buttonPane.setLayout(new BoxLayout(buttonPane, BoxLayout.LINE_AXIS));
@@ -77,8 +63,6 @@ public class AttendantCartScreen extends JPanel implements ListSelectionListener
         buttonPane.add(Box.createHorizontalStrut(5));
         buttonPane.add(removeButton);
         
-        //buttonPane.add(itemName);
-        //buttonPane.add(addButton);
         buttonPane.setBorder(BorderFactory.createEmptyBorder(5,5,5,5));
         c.weightx = 0.8;
         c.fill = GridBagConstraints.HORIZONTAL;
@@ -102,28 +86,18 @@ public class AttendantCartScreen extends JPanel implements ListSelectionListener
         }
     }
     class BackListener implements ActionListener {
-        private JButton button;
-
-        public BackListener(JButton button) {
-            this.button = button;
-        }
         public void actionPerformed(ActionEvent e) {
             //TODO: Back button implementation
         }
     }
     class RemoveListener implements ActionListener {
-        private JButton button;
-
-        public RemoveListener(JButton button) {
-            this.button = button;
-        }
         public void actionPerformed(ActionEvent e) {
             //This method can be called only if
             //there's a valid selection
             //so go ahead and remove whatever's selected.
             int index = items.getSelectedIndex();
-            //TODO: Remove Item from Checkout
             itemModel.remove(index);
+            //TODO: Remove Item from Checkout
 
             int size = itemModel.getSize();
 
@@ -139,85 +113,6 @@ public class AttendantCartScreen extends JPanel implements ListSelectionListener
                 items.setSelectedIndex(index);
                 items.ensureIndexIsVisible(index);
             }
-        }
-    }
-    class AddListener implements ActionListener, DocumentListener {
-        private boolean alreadyEnabled = false;
-        private JButton button;
-
-        public AddListener(JButton button) {
-            this.button = button;
-        }
-
-        //Required by ActionListener.
-        public void actionPerformed(ActionEvent e) {
-            String name = itemName.getText();
-
-            //User didn't type in a unique name...
-            if (name.equals("") || alreadyInList(name)) {
-                Toolkit.getDefaultToolkit().beep();
-                itemName.requestFocusInWindow();
-                itemName.selectAll();
-                return;
-            }
-
-            int index = items.getSelectedIndex(); //get selected index
-            if (index == -1) { //no selection, so insert at beginning
-                index = 0;
-            } else {           //add after the selected item
-                index++;
-            }
-
-            itemModel.insertElementAt(itemName.getText(), index);
-            //If we just wanted to add to the end, we'd do this:
-            //listModel.addElement(employeeName.getText());
-
-            //Reset the text field.
-            itemName.requestFocusInWindow();
-            itemName.setText("");
-
-            //Select the new item and make it visible.
-            items.setSelectedIndex(index);
-            items.ensureIndexIsVisible(index);
-        }
-
-        //This method tests for string equality. You could certainly
-        //get more sophisticated about the algorithm.  For example,
-        //you might want to ignore white space and capitalization.
-        protected boolean alreadyInList(String name) {
-            return itemModel.contains(name);
-        }
-
-        //Required by DocumentListener.
-        public void insertUpdate(DocumentEvent e) {
-            enableButton();
-        }
-
-        //Required by DocumentListener.
-        public void removeUpdate(DocumentEvent e) {
-            handleEmptyTextField(e);
-        }
-
-        //Required by DocumentListener.
-        public void changedUpdate(DocumentEvent e) {
-            if (!handleEmptyTextField(e)) {
-                enableButton();
-            }
-        }
-
-        private void enableButton() {
-            if (!alreadyEnabled) {
-                button.setEnabled(true);
-            }
-        }
-
-        private boolean handleEmptyTextField(DocumentEvent e) {
-            if (e.getDocument().getLength() <= 0) {
-                button.setEnabled(false);
-                alreadyEnabled = false;
-                return true;
-            }
-            return false;
         }
     }
 }
