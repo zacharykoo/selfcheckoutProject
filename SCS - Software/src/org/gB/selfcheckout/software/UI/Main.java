55eb8d81160a0045695e4145c3e8315c640d778d
MackenzieBowal <mackenzie.bowal@gmail.com>
2022-04-09 17:17:51 -0600

    Added attendant frame variable to main

--- SCS - Software/src/org/gB/selfcheckout/software/UI/Main.java
+++ SCS - Software/src/org/gB/selfcheckout/software/UI/Main.java
@@ -1,10 +1,11 @@
 package org.gB.selfcheckout.software.UI;
 
 public class Main {
-	public static CustomerFrame customerFrame;
+	
+	public static final AttendantFrame ATTENDANT_FRAME = new AttendantFrame();
 	
 	public static void main(String[] args) {		
-		customerFrame = new CustomerFrame(0);
+		CustomerFrame customerFrame = new CustomerFrame(0);
 		customerFrame.setSize(500,400);
 	}
 }
