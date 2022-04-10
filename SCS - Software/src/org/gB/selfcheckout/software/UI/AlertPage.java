4abe60598c0870191103e947bb9c8213af6b2788
Miro <47343299+nir04m@users.noreply.github.com>
2022-04-09 19:18:37 -0600

    Update AlertPage.java

--- SCS - Software/src/org/gB/selfcheckout/software/UI/AlertPage.java
+++ SCS - Software/src/org/gB/selfcheckout/software/UI/AlertPage.java
@@ -3,19 +3,19 @@ package org.gB.selfcheckout.software.UI;
 import javax.swing.*;  
 import java.awt.*;  
 import java.awt.event.*; 
-	public class AlertPage extends JFrame {
-		public static void main(String[] args) {
-			 JFrame f= new JFrame("Label Example");  
+	public class AlertPage extends JPanel {
+		public void alertPage() {
+			super();
 			    JLabel label;
 			    JButton button = new JButton("accept");  
 			    label=new JLabel("Alert.");  
 			    label.setBounds(50,50, 100,30);   
 			    button.setBounds(50,100, 100,30);  
-			    f.add(label); f.add(button);  
-			    f.setSize(300,300);  
-			    f.setLayout(null);  
-			    f.setVisible(true);  
+			    this.add(label); this.add(button);  
 			    
-		}  
+		} 
+		public setText(String msg) {
+			this.label.setText(msg);
+		}
         
 	}
