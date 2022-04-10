050880fbeab558cdaf90337641135964a986ef18
Miro <47343299+nir04m@users.noreply.github.com>
2022-04-09 19:20:32 -0600

    Update MemberInfo.java

--- SCS - Software/src/org/gB/selfcheckout/software/UI/MemberInfo.java
+++ SCS - Software/src/org/gB/selfcheckout/software/UI/MemberInfo.java
@@ -5,6 +5,7 @@ import java.awt.*;
 import java.awt.event.*; 
     public class MemberInfo extends JPanel implements ActionListener{
     	public void memberinfo() { 
+		super();
             JTextField t1;  
             t1=new JTextField("Enter your member info");  
             t1.setBounds(90,100, 250,30);
