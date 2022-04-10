1d7ad152df486948b669ab7357c940feab31cede
Colton Nguyen <advancewolf@gmail.com>
2022-04-09 22:58:28 -0600

    Add AttendantControlTest

---
+++ SCS - Software - Test/src/org/gB/selfcheckout/test/AttendantControlTest.java
@@ -0,0 +1,89 @@
+package org.gB.selfcheckout.test;
+
+import static org.junit.Assert.*;
+
+import org.gB.selfcheckout.software.AttendantControl;
+import org.gB.selfcheckout.software.State;
+
+import org.junit.Test;
+
+public class AttendantControlTest {
+	private State state;
+	public AttendantControl attendant;
+
+	@Test
+	public void testAttendantControl() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAttendantLogout() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testShutdownStation() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testStartupStation() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetSCSList() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddInkCartridge() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddPaper() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRefillCoinDispenser() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEmptyCoinStorageUnit() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRefillBanknoteDispenser() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEmptyBanknoteStorageUnit() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAttendantLooksUpProduct() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testBlockStation() {
+		
+	}
+
+	@Test
+	public void testAttendantRemoveProduct() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAttendantApproveWeightDifference() {
+		fail("Not yet implemented");
+	}
+
+}
