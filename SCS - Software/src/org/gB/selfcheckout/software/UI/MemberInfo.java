1cbb14b25c7020ba28c8f27ff306f2acb3194080
Miro <47343299+nir04m@users.noreply.github.com>
2022-04-09 19:15:19 -0600

    Update MemberInfo.java

--- SCS - Software/src/org/gB/selfcheckout/software/UI/MemberInfo.java
+++ SCS - Software/src/org/gB/selfcheckout/software/UI/MemberInfo.java
@@ -3,20 +3,16 @@ package org.gB.selfcheckout.software.UI;
 import javax.swing.*;  
 import java.awt.*;  
 import java.awt.event.*; 
-    public class MemberInfo extends JFrame implements ActionListener{
-    	public static void main(String[] args) {
-    		JFrame frame= new JFrame("Membership Info");  
+    public class MemberInfo extends JPanel implements ActionListener{
+    	public void memberinfo() { 
             JTextField t1;  
             t1=new JTextField("Enter your member info");  
             t1.setBounds(90,100, 250,30);
             JLabel l1;
     	    l1=new JLabel("Member info :");
     	    l1.setBounds(10,105, 190,20);
-    	    frame.add(l1);
-            frame.add(t1);  
-            frame.setSize(400,400);  
-            frame.setLayout(null);  
-            frame.setVisible(true); 
+    	    this.add(l1);
+            this.add(t1);  
     	}
     	@Override
 		public void actionPerformed(ActionEvent e) {
