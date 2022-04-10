516d2da56f1e1c7c01225b9355319ec886731078
tim-lerm <timofei.lerman@ucalgary.ca>
2022-04-09 18:04:17 -0600

    Merge branch 'Backend/Usecase-Group-1'

--- SCS - Software - Test/src/org/gB/selfcheckout/test/TestCustomerDoesNotWantToBagScannedItem.java
+++ SCS - Software - Test/src/org/gB/selfcheckout/test/TestCustomerDoesNotWantToBagScannedItem.java
@@ -6,8 +6,10 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.lsmr.selfcheckout.Item;
+
 public class TestCustomerDoesNotWantToBagScannedItem {
 	private State state;
+	
 	
 	@Before
 	public void setup() throws Exception {
@@ -32,6 +34,7 @@ public class TestCustomerDoesNotWantToBagScannedItem {
 		Assert.assertFalse(state.scs.handheldScanner.isDisabled());
 	}
 	
+	@Test
 	public void testValidDisabledWeightChange() {
 		Item i = new Item(45.6) {};
 		// "Scan" an item.
@@ -47,5 +50,99 @@ public class TestCustomerDoesNotWantToBagScannedItem {
 		Assert.assertTrue(state.scs.mainScanner.isDisabled());
 		Assert.assertTrue(state.scs.handheldScanner.isDisabled());
 	}
+	
+	@Test
+	public void testInvalidWeightChange() {
+		Item i1 = new Item(45.6) {};
+		Item i2 = new Item(22.3) {};
+		// "Scan" item 'a'.
+		state.addItem(i1);
+		state.scs.mainScanner.disable();
+		state.scs.handheldScanner.disable();
+		// "Weight" item 'b'.
+		state.scs.baggingArea.add(i2);
+		// Ensure the state is not reversed to a scanning mode.
+		Assert.assertTrue(state.scs.mainScanner.isDisabled());
+		Assert.assertTrue(state.scs.handheldScanner.isDisabled());
+	}
+	
+	@Test
+	public void testUnexpectedChange() {
+		Item i = new Item(12.66) {};
+		state.scs.baggingArea.add(i);
+		Assert.assertFalse(state.scs.mainScanner.isDisabled());
+		Assert.assertFalse(state.scs.handheldScanner.isDisabled());
+	}
+	
+	@Test
+	public void testValidAfterOverload() {
+		Item i1 = new Item(45.6) {};
+		Item i2 = new Item(20000) {};
+		// "Scan" an item.
+		state.addItem(i1);
+		
+		state.scs.mainScanner.disable();
+		state.scs.handheldScanner.disable();
+		// Put a spurious overloading item on the scale.
+		state.scs.baggingArea.add(i2);
+		
+		Assert.assertTrue(state.scs.mainScanner.isDisabled());
+		Assert.assertTrue(state.scs.handheldScanner.isDisabled());
+		state.scs.baggingArea.add(i1); // Add the scanned item.
+		
+		Assert.assertTrue(state.scs.mainScanner.isDisabled());
+		Assert.assertTrue(state.scs.handheldScanner.isDisabled());
+		state.scs.baggingArea.remove(i2); // Remove the spurious item.
+		// Ensure the state is correctly updated.
+		
+		Assert.assertFalse(state.scs.mainScanner.isDisabled());
+		Assert.assertFalse(state.scs.handheldScanner.isDisabled());
+	}
+	
+	
+	@Test
+	public void overloadCycle() {
+		Item i = new Item(20000) {};
+		state.scs.baggingArea.add(i);
+		Assert.assertFalse(state.scs.mainScanner.isDisabled());
+		Assert.assertFalse(state.scs.handheldScanner.isDisabled());
+		state.scs.baggingArea.remove(i);
+		Assert.assertFalse(state.waitingForBagging);
+		Assert.assertFalse(state.scs.mainScanner.isDisabled());
+		Assert.assertFalse(state.scs.handheldScanner.isDisabled());
+	}
+	
+	public void addItemWhileDisabled() {
+		Item i = new Item(45.6) {};
+		state.scs.baggingArea.disable(); // Disable the scale.
+		// "Scan" an item.
+		state.addItem(i);
 
+		state.scs.mainScanner.disable();
+		state.scs.handheldScanner.disable();
+		state.scs.baggingArea.add(i);
+		
+		Assert.assertTrue(state.scs.mainScanner.isDisabled());
+		Assert.assertTrue(state.scs.handheldScanner.isDisabled());
+	}
+	
+	
+
+	
+	
+	
+/**
+	@Test
+	public void testEnabled() {
+		state.customerDoesNotWantToBagScannedItem.enabled(null);
+		Assert.assertFalse(state.scs.baggingArea.isDisabled());
+	}
+	
+	@Test
+	public void testNotEnabled() {
+		state.customerDoesNotWantToBagScannedItem.disabled(null);
+		Assert.assertTrue(state.scs.baggingArea.isDisabled());
+	}
+	**/
+	
 }
