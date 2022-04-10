29eb3c073fe4be7cb84eb551649fb3906c5b61b8
Miro <47343299+nir04m@users.noreply.github.com>
2022-04-09 19:19:47 -0600

    Update CustomerWarning.java

--- SCS - Software/src/org/gB/selfcheckout/software/UI/CustomerWarning.java
+++ SCS - Software/src/org/gB/selfcheckout/software/UI/CustomerWarning.java
@@ -5,10 +5,12 @@ import java.awt.*;
 import java.awt.event.*; 
 public class CustomerWarning extends JPanel {
 	public void warningPage() {
+		super();
 	      JLabel label = new JLabel("Warning Please wait for an employee to attend to you");
 	      this.setLayout(new GridBagLayout());
 	      this.add(label);
 	   }
-	}  
-
-}
+	public setText(String msg) {
+		this.label.setText(msg);
+	}
+}  
